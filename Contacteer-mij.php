d616cac499d5299f77cc14500ce94f075dbf1263
Matthew De Schepper <r0903037@student.thomasmore.be>
2021-10-31 19:40:17 +0100

    contactformulier

--- Contacteer-mij.php
+++ Contacteer-mij.php
@@ -19,6 +19,35 @@
 <p>School E-mail:	test@email.com</p>
 <p>telefoonnummer:	+32499999999</p>
 <p>Nog geen persoonlijke gegevens vermits dit een testproject is.</p>
-<p>Meer volgt later...</p>
+<form action="post-demo.php" method="post">
+      <fieldset>
+        <legend>Persoonlijke gegevens</legend>
+        <label for="firstName">Voornaam</label>
+        <input type="text" name="Voornaam" id="firstName" /><br />
+        <label for="lastName">Familienaam</label>
+        <input type="text" name="Familienaam" id="lastName" /><br />
+        <label for="email">E-mail</label>
+        <input type="email" name="E-mail" id="email" />
+      </fieldset>
+      <fieldset>
+        <legend>Uw vraag</legend>
+        <label for="questionType">Soort vraag</label>
+        <select name="Type-vraag" id="questionType">
+          <option value="algemeen">Algemene vraag</option>
+          <option value="offerte">Offerte aanvragen</option>
+          <option value="andere">Andere vraag</option></select
+        ><br />
+        <label for="question">Uw vraag</label>
+        <textarea name="Vraag" id="question" cols="30" rows="10"></textarea>
+      </fieldset>
+      <label for="newsletter">Onze nieuwsbrief ontvangen?</label>
+      <input
+        type="checkbox"
+        name="Nieuwsbrief"
+        id="newsletter"
+        value="ja"
+      /><br />
+      <input type="submit" value="Verstuur" />
+    </form>
 </body>
 </html>
