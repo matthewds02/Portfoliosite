4f4c4562d7f9f752333cfbb53657ce871e0ad308
matthewds02 <descheppermatthew@gmail.com>
2022-09-25 22:13:46 +0200

    portfolio website

---
+++ V4-responsive/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Info over mij</title>
+		<link type="text/css" rel="stylesheet" href="lib/stylesheet-php.css"/>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+	</head>
+	<body>
+		<header class="site-header">
+			<?php include 'lib/header.php';?>
+		</header>
+		<div class="flex-container">
+			<main>
+				<h1>Homepage</h1>
+				<p>Ik ben Matthew De Schepper, geboren op 20/12/2002 te Brasschaat.</p>
+				<p>Ik ben een student in Graduaat programmeren aan Thomas More de Hogeschool in Antwerpen.</p>
+				<p>Op de volgende pagina's zult u meer informatie over mij en projecten waaraan ik gewerkt heb kunnen vinden.</p>
+			</main>
+		</div>
+	</body>
+</html>
---
+++ V4-responsive/ov_proj.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Overzicht Projecten</title>
+		<link type="text/css" rel="stylesheet" href="lib/stylesheet-php.css"/>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+	</head>
+	<body>
+		<header class="site-header">
+			<?php include 'lib/header.php';?>
+		</header>
+		<div class="flex-container">
+			<main>
+				<div class="eentweede">
+					<img src="afbeeldingen/bootstrappage2.png" alt="foto van website">
+					<div class="container">
+						<a  class="noline" href="/bootstrap-website/index.html"><h4><b>Website</b></h4>
+						<p>Dit is de website voor mijn portfolio, klik voor meer info.</p></a>
+  					</div>
+				</div>
+				<div class="eentweede">
+					<img src="afbeeldingen/Build-a-City2.png" alt="foto van het project">
+					<div class="container">
+						<a  class="noline" href="det_proj.php"><h4><b>Build-a-City</b></h4></a>
+  					</div>
+				</div>
+				<p>Of ga naar mijn <a class="speciallink" href="https://github.com/matthewds02" target="_blank">Github account</a> om alle projecten en code te bekijken.</p>
+			</main>
+		</div>
+	</body>
+</html>
